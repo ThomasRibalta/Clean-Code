70fc917684b02ac97ebe117308fa80b0142a8292
Thomas Ribalta <thomas.rba@Ordinateur-portable-de-Thomas.local>
2024-01-22 10:15:50 +0100

    doublons revu

--- Nom de Variable/revelant.c
+++ Nom de Variable/revelant.c
@@ -1,44 +1,21 @@
-#include <stdio.h>
-#include <string.h>
+// Mauvais exemple
+int fonction1(char *m){
+    int i;
 
-#define MAX_FRUITS 5
-#define MAX_FRUIT_NAME 10
-
-void insert(char array[MAX_FRUITS][MAX_FRUIT_NAME], int *size, const char *fruit) {
-    if (*size >= MAX_FRUITS) {
-        printf("Le tableau est plein. Impossible d'ajouter un nouvel élément.\n");
-        return;
+    i = 0;
+    while (m[i]){
+        i++;
     }
-    int i = *size;
-    while (i > 0) {
-        strcpy(array[i], array[i - 1]);
-        i--;
-    }
-    strcpy(array[0], fruit);
-    (*size)++;
+    return (i);
 }
 
-void append(char array[MAX_FRUITS][MAX_FRUIT_NAME], int *size, const char *fruit) {
-    if (*size >= MAX_FRUITS) {
-        printf("Le tableau est plein. Impossible d'ajouter un nouvel élément.\n");
-        return;
+// Bon exemple
+int longueurDuMot(char *mot){
+    int nombreDeLettre;
+
+    nombreDeLettre = 0;
+    while (mot[nombreDeLettre]){
+        nombreDeLettre++;
     }
-    strcpy(array[*size], fruit);
-    (*size)++;
-}
-
-int main() {
-    char ArrayFruits[MAX_FRUITS][MAX_FRUIT_NAME];
-    int size = 0;
-
-    appendAtEnd(ArrayFruits, &size, "Pomme");
-    append(ArrayFruits, &size, "Banane");
-    append(ArrayFruits, &size, "Orange");
-    printf("Tableau actuel : ");
-    displayArray(ArrayFruits, size);
-    insert(ArrayFruits, &size, "Fraise");
-    printf("Tableau après insertion au début : ");
-    displayArray(ArrayFruits, size);
-
-    return 0;
+    return (nombreDeLettre);
 }
