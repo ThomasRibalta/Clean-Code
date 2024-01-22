04684a99df64a0a1d3fab88b5f1f159a1cb29143
Thomas Ribalta <thomas.rba@Ordinateur-portable-de-Thomas.local>
2024-01-22 10:13:52 +0100

    doublons revu

--- Nom de Variable/doublons.c
+++ Nom de Variable/doublons.c
@@ -1,21 +1,84 @@
 #include <stdio.h>
+#include <stdio.h>
+#include <string.h>
 
-// Mauvais exemple
+#define MAX_FRUITS 5
+#define MAX_FRUIT_NAME 10
+// Bon exemple
+void insert(char array[MAX_FRUITS][MAX_FRUIT_NAME], int *size, const char *fruit) {
+    if (*size >= MAX_FRUITS) {
+        printf("Le tableau est plein. Impossible d'ajouter un nouvel élément.\n");
+        return;
+    }
+    int i = *size;
+    while (i > 0) {
+        strcpy(array[i], array[i - 1]);
+        i--;
+    }
+    strcpy(array[0], fruit);
+    (*size)++;
+}
+
+void append(char array[MAX_FRUITS][MAX_FRUIT_NAME], int *size, const char *fruit) {
+    if (*size >= MAX_FRUITS) {
+        printf("Le tableau est plein. Impossible d'ajouter un nouvel élément.\n");
+        return;
+    }
+    strcpy(array[*size], fruit);
+    (*size)++;
+}
+
 int main() {
-    int nombre = 5;
-    int valeurFinal = nombre + 1;
+    char ArrayFruits[MAX_FRUITS][MAX_FRUIT_NAME];
+    int size = 0;
 
-    printf("La valeur est : %d\n", valeurFinal);
-
+    append(ArrayFruits, &size, "Pomme");
+    append(ArrayFruits, &size, "Banane");
+    append(ArrayFruits, &size, "Orange");
+    printf("Tableau actuel : ");
+    displayArray(ArrayFruits, size);
+    insert(ArrayFruits, &size, "Fraise");
+    printf("Tableau après insertion au début : ");
+    displayArray(ArrayFruits, size);
     return 0;
 }
 
 // Bon exemple
+
+void appendAtStart(char array[MAX_FRUITS][MAX_FRUIT_NAME], int *size, const char *fruit) {
+    if (*size >= MAX_FRUITS) {
+        printf("Le tableau est plein. Impossible d'ajouter un nouvel élément.\n");
+        return;
+    }
+    int i = *size;
+    while (i > 0) {
+        strcpy(array[i], array[i - 1]);
+        i--;
+    }
+    strcpy(array[0], fruit);
+    (*size)++;
+}
+
+void appendAtEnd(char array[MAX_FRUITS][MAX_FRUIT_NAME], int *size, const char *fruit) {
+    if (*size >= MAX_FRUITS) {
+        printf("Le tableau est plein. Impossible d'ajouter un nouvel élément.\n");
+        return;
+    }
+    strcpy(array[*size], fruit);
+    (*size)++;
+}
+
 int main() {
-    int nombre = 5;
+    char ArrayFruits[MAX_FRUITS][MAX_FRUIT_NAME];
+    int size = 0;
 
-    nombre++;
-    printf("La valeur est : %d\n", nombre);
-
+    appendAtEnd(ArrayFruits, &size, "Pomme");
+    appendAtEnd(ArrayFruits, &size, "Banane");
+    appendAtEnd(ArrayFruits, &size, "Orange");
+    printf("Tableau actuel : ");
+    displayArray(ArrayFruits, size);
+    appendAtStart(ArrayFruits, &size, "Fraise");
+    printf("Tableau après insertion au début : ");
+    displayArray(ArrayFruits, size);
     return 0;
-} 
+}
